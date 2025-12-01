b2835262ae602bde69e318b44d4b1d657a4d2120
TamasVencel3 <tamasvenci46@gmail.com>
2025-12-01 15:25:25 +0200

    initial commit

---
+++ day_03.py
@@ -0,0 +1,13 @@
+def part1():
+    with open("inputs/day_03.txt") as f:
+        data = f.read().strip()
+    pass
+
+def part2():
+    with open("inputs/day_03.txt") as f:
+        data = f.read().strip()
+    pass
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
